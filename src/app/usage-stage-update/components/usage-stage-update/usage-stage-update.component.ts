2fd4afc8fd4055d91a8a38a91bce43c4e67d5dec
jcarlosgs02 <89940978+jcarlosgs02@users.noreply.github.com>
2021-10-19 14:22:36 -0500

    Update usage-stage-update.component.ts

--- src/app/usage-stage-update/components/usage-stage-update/usage-stage-update.component.ts
+++ src/app/usage-stage-update/components/usage-stage-update/usage-stage-update.component.ts
@@ -211,7 +211,15 @@ export class UsageStageUpdateComponent implements OnInit {
       })
     );
   }
-
+  
+  var delayTimer;
+  function input(ele) {
+    clearTimeout(delayTimer);
+    delayTimer = setTimeout(function() {
+       ele.value = parseFloat(ele.value).toFixed(2).toString();
+    }, 1100); 
+  }
+  
   goToMaterialStage() {
     this.router.navigateByUrl('materials-stage');
   }
