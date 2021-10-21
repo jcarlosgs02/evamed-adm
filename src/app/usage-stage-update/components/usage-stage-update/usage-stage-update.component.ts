349dcb6a000c7c663fceae5e42cbb3add3668900
jcarlosgs02 <89940978+jcarlosgs02@users.noreply.github.com>
2021-10-21 15:18:17 -0500

    Update usage-stage-update.component.ts

--- src/app/usage-stage-update/components/usage-stage-update/usage-stage-update.component.ts
+++ src/app/usage-stage-update/components/usage-stage-update/usage-stage-update.component.ts
@@ -141,7 +141,7 @@ export class UsageStageUpdateComponent implements OnInit {
 
   changePorcentajeME(porcentajeMixElectrico) {
     this.porcentajeMixElectrico =porcentajeMixElectrico.toFixed(2).toString();
-    this.cantidadMixElectrico = Math.round(((((porcentajeMixElectrico * this.cantidad) / 100) * 100) / 100);
+    this.cantidadMixElectrico = Math.round((((porcentajeMixElectrico * this.cantidad) / 100) * 100) / 100);
     
     this.suma=+this.porcentajeMixElectrico + +this.porcentajeCombustible + +this.porcentajePanelesFotovoltaicos;
     
