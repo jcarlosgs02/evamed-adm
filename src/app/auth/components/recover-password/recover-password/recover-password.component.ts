cfa0adb8cf8a7c1c646f62f013f9a4223c1a4869
jcarlosgs02 <89940978+jcarlosgs02@users.noreply.github.com>
2021-11-02 13:44:41 -0600

    Update recover-password.component.ts

--- src/app/auth/components/recover-password/recover-password/recover-password.component.ts
+++ src/app/auth/components/recover-password/recover-password/recover-password.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 @Component({
   selector: 'app-recover-password',
@@ -22,13 +23,14 @@ export class RecoverPasswordComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
   private buildForm() {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required]],
-      password: ['', [Validators.required]],
+      
     });
   }
+
 
   register() {
     this.router.navigate(['/auth/register']);
@@ -37,8 +39,14 @@ export class RecoverPasswordComponent implements OnInit {
   recovery(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
-      console.log('recuperar password');
+      const value = this.form.value;
+      this.authService.resetPassword(value.email)
+      .then( () => {
+        
+        this.router.navigate(['/']);
+      })
+      .catch( () => {
+        alert('Error! Intenta nuevamente la solicitud');
+      });
+     }
     }
-  }
-
-}
