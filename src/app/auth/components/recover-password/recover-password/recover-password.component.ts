ab35c15cd84311ef0103d0e5e07454482e32ad61
jcarlosgs02 <89940978+jcarlosgs02@users.noreply.github.com>
2021-11-02 13:53:11 -0600

    Update recover-password.component.ts

--- src/app/auth/components/recover-password/recover-password/recover-password.component.ts
+++ src/app/auth/components/recover-password/recover-password/recover-password.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+
 
 @Component({
   selector: 'app-recover-password',
